5c9ba0a44c5694d9fa9184b78a0fea776c3b762f
김민선 <122509421+minsun-cloud@users.noreply.github.com>
2024-09-25 09:44:12 +0900

    Title: 따옴표 출력, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

--- 240925/따옴표 출력/print-quote.java
+++ 240925/따옴표 출력/print-quote.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("/"It's a really simple sentnce/".")
+        System.out.print("He says \"It's a really simple sentence\".");
     }
 }
