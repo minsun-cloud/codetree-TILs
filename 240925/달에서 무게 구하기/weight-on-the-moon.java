566d4c9c222131d4cf875098326092acdd817bc3
김민선 <122509421+minsun-cloud@users.noreply.github.com>
2024-09-25 11:17:40 +0900

    Title: 달에서 무게 구하기, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240925/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 13;
+        double g = 0.165;
+        double y = x * g;
+        System.out.printf("%d * %f = %.6f", x,g,y);
+    }
+}
