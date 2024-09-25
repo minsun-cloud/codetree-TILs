a2204e402dccc9de3705df5f47a0acc0119b7087
김민선 <122509421+minsun-cloud@users.noreply.github.com>
2024-09-25 14:25:17 +0900

    Title: 변수 값 교체하기 3, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240925/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+        int c;
+        c=b;
+        b=a;
+        a=c;
+        System.out.printf("%d\n%d",a,b);
+    }
+}
