6b2fe41190fd32957af86e920fab47ad0fdfdb3b
김민선 <122509421+minsun-cloud@users.noreply.github.com>
2024-09-26 23:39:08 +0900

    Title: 세 실수의 반올림, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240926/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextDouble();
+        float b = sc.nextDouble();
+        float c = sc.nextDouble();
+        System.out.printf("%.3f\n%.3f\n%.3f",a,b,c);
+    }
+}
